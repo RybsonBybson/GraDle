db2d57b10a1dd5689b81294aa97ad221de8178f2
RybsonBybson <miwlod08+1@gmail.com>
2025-10-08 17:13:36 +0200

    ..

--- main/src/main.jsx
+++ main/src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./style.css";
 import { Toaster } from "react-hot-toast";
-import Offcanvasbar from "./templates/offcanvasbar.jsx";
 
 const rootElement = document.getElementById("root");
 
@@ -11,7 +9,6 @@ if (rootElement)
   createRoot(rootElement).render(
     <StrictMode>
       <Toaster position="top-right" />
-      <Offcanvasbar />
     </StrictMode>
   );
 else console.error("Couldn't find root!");
